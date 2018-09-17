4a7d4ffcc5564337d0ed297955308c482001aa03
tpphu <phutp@sendo.vn>
2018-09-17 16:53:22 +0700

    * Update code

---
+++ examples/local/test.sh
@@ -0,0 +1,15 @@
+hostname=`hostname -f`
+vtctld_web_port=15000
+
+# Set up environment.
+export VTTOP=$VTROOT/src/vitess.io/vitess
+
+# Try to find mysqld_safe on PATH.
+if [ -z "$VT_MYSQL_ROOT" ]; then
+  mysql_path=`which mysqld_safe`
+  if [ -z "$mysql_path" ]; then
+    echo "Can't guess location of mysqld_safe. Please set VT_MYSQL_ROOT so it can be found at \$VT_MYSQL_ROOT/bin/mysqld_safe."
+    exit 1
+  fi
+  export VT_MYSQL_ROOT=$(dirname `dirname $mysql_path`)
+fi
